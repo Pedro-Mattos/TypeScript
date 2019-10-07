68bedb835fc7aed7d9bfa4b8f2cb0b1ed3479fca
Pedro de Mattos <pedrodemattos86@gmail.com>
2019-10-07 12:18:08 -0300

    trabalhando com template String, retornando a view a tabela com as negociacoes após inclusão de novas negociacoes

--- app/js/controllers/NegociacaoController.js
+++ app/js/controllers/NegociacaoController.js
@@ -2,18 +2,17 @@ class NegociacaoController {
     constructor() {
         this._negociacoes = new Negociacoes();
         this._negociacoesView = new NegociacoesView('#negociacoesView');
-        //fizemos um casting para o tipo htmlImputElement pois estamos do especifico para o geral que seria um element.
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
-        this._negociacoesView.update();
+        // atualiza a view para exibir os dados do modelo, vazio
+        this._negociacoesView.update(this._negociacoes);
     }
-    //tipei o event para o tipo Event para que o ts perceba.
     adiciona(event) {
         event.preventDefault();
-        //conversao em string para date, string para inteiro e para float no caso de valor
         const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
         this._negociacoes.adiciona(negociacao);
-        console.log(negociacao);
+        // depois de adicionar, atualiza a view novamente para refletir os dados
+        this._negociacoesView.update(this._negociacoes);
     }
 }
